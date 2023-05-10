264ca8bd9cf6069d1e3f51f2eeb4247f7c1c59dc
Maria <maria.shn.es@gmail.com>
2023-05-10 20:54:24 +0300

    started typing code

---
+++ src/target/classes/Sort.java
@@ -0,0 +1,40 @@
+public class Sort <T extends comparable<T>> {
+    public void quickSortClass(T[] array){
+        
+    }
+
+    public void quickSortRecitation(T[] array){
+
+    }
+
+    public static void radixSort(int[] array, int base){
+
+    }
+    
+    public void mergeSortRecursive(T[] array){
+
+    }
+
+    public void mergeSortIterative(T[] array){
+
+    }
+
+    public void setNaiveSortThreshold(int threshold){
+
+    }
+
+    public void quickSortClass(T[] array, int first, int last){
+        if (first - last > 3){
+            int q = partition(T[] array, int p, int r);
+        }
+    }
+
+    public int partition(T[] array, int p, int r){
+        int x = A[r];
+        int j = r;
+        int i = p-1;
+        while(true){
+            while()
+        }
+    }
+}
