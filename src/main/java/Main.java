7e38b16f778a72fdbf69f15c5a38fc8bfd1c108c
Maria <maria.shn.es@gmail.com>
2023-05-18 22:53:01 +0300

    this one is good

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,8 @@ public class Main {
             a[i] = j;
             j++;
         }
-        Integer[] arr = {2134, 346623, 9123, 349, -314, 384, 45, 53, 2034, 5595, 285, 5996, 0};
+        //Integer[] arr = {2134, 346623, 9123, 349, -314, 384, 45, 53, 2034, 5595, 285, 5996, 0};
+        Integer[] arr = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         Sort s = new Sort();
         s.mergeSortIterative(arr);
         System.out.print("{ ");
